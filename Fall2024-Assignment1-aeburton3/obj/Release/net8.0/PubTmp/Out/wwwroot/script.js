c601dfedb95d79016baf14c879b31c049b9e4940
Ellie Burton <aeburton3@crimson.ua.edu>
2024-09-22 18:52:13 -0500

    code

---
+++ Fall2024-Assignment1-aeburton3/obj/Release/net8.0/PubTmp/Out/wwwroot/script.js
@@ -0,0 +1,15 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const leafCount = 10; 
+    const leafContainer = document.querySelector(".falling-leaves-container");
+
+    for (let i = 0; i < leafCount; i++) {
+        let leaf = document.createElement("div");
+        leaf.classList.add("leaf");
+        leaf.style.left = Math.random() * 100 + "vw";
+        let fallDuration = Math.random() * 5 + 5;
+        let fallDelay = Math.random() * 5;
+        leaf.style.animationDuration = fallDuration + "s";
+        leaf.style.animationDelay = fallDelay + "s";
+        leafContainer.appendChild(leaf);
+    }
+});
